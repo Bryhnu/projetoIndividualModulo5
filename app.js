81161f335fc20c4ac0fbd460126698005cce12b0
Bruno Pereira <brunocarvpinto@gmail.com>
2023-02-13 14:15:48 -0300

    Add files via upload
    primeiro commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const redlineSync = require("readline-sync");
+
+const lista = [];
+var resp = "";
+
+const adicionarNaLista = (prop) => {
+    lista.push(prop);
+};
+
+const ordenar = () => {
+    lista.sort();
+};
+
+const listar = () => {
+    //console.log(lista); //Lista como objeto array
+    //console.log(lista.toString()); //Lista array de string
+    lista.forEach((elemento) => { console.log(elemento); }); //Lista por linha como string
+};
+
+while (true) {
+    resp = redlineSync.question("Digite a propriedade CSS ou 'SAIR' para exibir a listagem e encerrar: ");
+
+    if (resp.toLowerCase() == "sair") {
+        ordenar();
+        listar();
+        break;
+
+    } else if (resp.toLowerCase() !== "sair" && resp !== "") {
+        adicionarNaLista(resp);
+    }
+}
+
+
